4d40eade287d8373e28a617c940da874bed31acf
Jan Kara <jack@suse.cz>
2016-08-09 18:44:23 +0200

    - xfs: Silence warnings in xfs_vm_releasepage() (bnc#915183   bsc#987565). - Delete patches.suse/xfs-skip-dirty-pages-in-releasepage.patch.
    suse-commit: d545eae86f6b32440d4607a2be8d204cbc6191aa

--- fs/xfs/xfs_aops.c
+++ fs/xfs/xfs_aops.c
@@ -1179,15 +1179,16 @@ xfs_vm_releasepage(
 
 	trace_xfs_releasepage(page->mapping->host, page, 0, 0);
 
-	if (PageDirty(page))
-		return 0;
-
 	xfs_count_page_state(page, &delalloc, &unwritten);
 
-	if (WARN_ON(delalloc))
-		return 0;
-	if (WARN_ON(unwritten))
-		return 0;
+	if (delalloc) {
+		WARN_ON_ONCE(!PageDirty(page));
+ 		return 0;
+	}
+	if (unwritten) {
+		WARN_ON_ONCE(!PageDirty(page));
+ 		return 0;
+	}
 
 	return try_to_free_buffers(page);
 }
