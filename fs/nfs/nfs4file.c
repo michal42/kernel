06005fb79cbe11f3a807322151c6b82ba02df973
NeilBrown <neilb@suse.com>
2016-10-20 11:22:13 +1100

    Fix kabi change cause by adding flock_owner to open_context (bsc#998689).
    suse-commit: 51671c87d1e94120d70c7d76c08679916000f4a2

--- fs/nfs/nfs4file.c
+++ fs/nfs/nfs4file.c
@@ -44,10 +44,11 @@ nfs4_file_open(struct inode *inode, struct file *filp)
 	parent = dget_parent(dentry);
 	dir = parent->d_inode;
 
-	ctx = alloc_nfs_open_context(filp->f_path.dentry, filp->f_mode, filp);
+	ctx = alloc_nfs_open_context(filp->f_path.dentry, filp->f_mode);
 	err = PTR_ERR(ctx);
 	if (IS_ERR(ctx))
 		goto out;
+	ctx->flock_owner = (fl_owner_t)filp;
 
 	attr.ia_valid = ATTR_OPEN;
 	if (openflags & O_TRUNC) {
