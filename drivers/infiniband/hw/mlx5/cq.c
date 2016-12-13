818afe6b7084b6c4fa22b4b4ddd5f9282df57e82
Benjamin Poirier <bpoirier@suse.com>
2016-12-12 17:23:16 -0800

    IB/mlx5: Fix entries checks in mlx5_ib_create_cq (bnc#858727 FATE#315946).
    suse-commit: 6d7a3130f5b84103f8924d30e3df52f6cd652ff1

--- drivers/infiniband/hw/mlx5/cq.c
+++ drivers/infiniband/hw/mlx5/cq.c
@@ -749,7 +749,8 @@ struct ib_cq *mlx5_ib_create_cq(struct ib_device *ibdev, int entries,
 	int eqn;
 	int err;
 
-	if (entries < 0)
+	if (entries < 0 ||
+	    (entries > (1 << MLX5_CAP_GEN(dev->mdev, log_max_cq_sz))))
 		return ERR_PTR(-EINVAL);
 
 	entries = roundup_pow_of_two(entries + 1);
