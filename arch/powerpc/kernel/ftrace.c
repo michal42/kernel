d96e8a7f8bd643b0a5e27ce5e7a2792020b5d863
Torsten Duwe <duwe@suse.de>
2017-04-11 15:48:29 +0200

    powerpc/module: Mark module stubs with a magic value (FATE#322421).
    suse-commit: df0c495091456a6a66a6f8b2e0d7aa8a9a0a6224

--- arch/powerpc/kernel/ftrace.c
+++ arch/powerpc/kernel/ftrace.c
@@ -104,10 +104,9 @@ static int
 __ftrace_make_nop(struct module *mod,
 		  struct dyn_ftrace *rec, unsigned long addr)
 {
-	unsigned int op;
-	unsigned long entry, ptr;
+	unsigned long entry, ptr, tramp;
 	unsigned long ip = rec->ip;
-	void *tramp;
+	unsigned int op;
 
 	/* read where this goes */
 	if (probe_kernel_read(&op, (void *)ip, sizeof(int)))
@@ -120,14 +119,9 @@ __ftrace_make_nop(struct module *mod,
 	}
 
 	/* lets find where the pointer goes */
-	tramp = (void *)find_bl_target(ip, op);
+	tramp = find_bl_target(ip, op);
 
-	pr_devel("ip:%lx jumps to %p", ip, tramp);
-
-	if (!is_module_trampoline(tramp)) {
-		printk(KERN_ERR "Not a trampoline\n");
-		return -EINVAL;
-	}
+	pr_devel("ip:%lx jumps to %lx", ip, tramp);
 
 	if (module_trampoline_target(mod, tramp, &ptr)) {
 		printk(KERN_ERR "Failed to get trampoline target\n");
